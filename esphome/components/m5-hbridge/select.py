eb4a555ec6bf5bedbe4e20456f0a8dd0cf556fbe
Nick Johnson <arachnid@notdot.net>
2024-10-02 13:32:28 +0100

    Default for select

--- esphome/components/m5-hbridge/select.py
+++ esphome/components/m5-hbridge/select.py
@@ -28,5 +28,6 @@ CONFIG_SCHEMA = select.select_schema(HBridgeDirection, entity_category=ENTITY_CA
 async def to_code(config):
     parent = await cg.get_variable(config[CONF_HBRIDGE_ID])
     sel = await select.new_select(config, options=[CONF_SELECTS])
+    cg.add(sel.publish_state("Forward"))
     await cg.register_parented(sel, config[CONF_HBRIDGE_ID])
     return sel
